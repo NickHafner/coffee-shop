8680fc5f57674b8e89a98718967d77df009d1d11
NickHafner <nickhafner@outlook.com>
2022-02-03 19:51:02 -0600

    Initializing Project

---
+++ CoffeeCommons/build.gradle.kts
@@ -0,0 +1,21 @@
+plugins {
+    kotlin("jvm") version "1.6.10"
+    java
+}
+
+group = "misc.group"
+version = "0.0.1-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    implementation(kotlin("stdlib"))
+    testImplementation("org.junit.jupiter:junit-jupiter-api:5.6.0")
+    testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine")
+}
+
+tasks.getByName<Test>("test") {
+    useJUnitPlatform()
+}
---
+++ src/main/kotlin/misc/group/coffee/shop/Application.kt
@@ -0,0 +1,11 @@
+package misc.group.coffee.shop
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class Application
+
+fun main(args: Array<String>) {
+	runApplication<Application>(*args)
+}
